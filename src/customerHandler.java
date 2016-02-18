17d5b28aa34ace2dde8f3db2bb5f3649a693cd36
Ali Karami <Ali.karami.m@gmail.com>
2016-02-18 12:06:10 +0330

    just checked

--- src/customerHandler.java
+++ src/customerHandler.java
@@ -1,5 +1,10 @@
 /**
  * Created by ali on 2/18/16.
  */
-public class customerHandler {
+
+import ir.ramtung.coolserver.*;
+import java.io.*;
+import java.util.*;
+import com.sun.net.httpserver.*;
+abstract class customerHandler implements ServiceHandler {
 }
