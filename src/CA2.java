0f63a79351cd076f85ef6985959b6a2c2d55d079
mohammad <mohi_6038550@yahoo.com>
2016-02-19 23:29:09 +0330

    rizehkari and stuff

--- src/CA2.java
+++ src/CA2.java
@@ -4,6 +4,28 @@ import java.util.*;
 import com.sun.net.httpserver.*;
 import ir.ramtung.coolserver.*;
 
+class ErrorHandler extends ServiceHandler{
+
+    public void execute(PrintWriter out){
+        out.println("Unknown command");
+    }
+    public void handle(HttpExchange t) throws IOException{
+        StringWriter sw = new StringWriter();
+        execute(new PrintWriter(sw, true));
+        String err = sw.toString();
+        byte[] result = err.getBytes();
+        t.sendResponseHeaders(404, result.length);
+        Headers headers = t.getResponseHeaders();
+        headers.add("Date", Calendar.getInstance().getTime().toString());
+        headers.add("Content-Type", "text/html");
+        OutputStream os = t.getResponseBody();
+        os.write(result);
+        os.close();
+    }
+}
+
+
+
 class Sell extends OrderHandler {
     Sell(Database db_) {
         db = db_;
@@ -11,8 +33,12 @@ class Sell extends OrderHandler {
     public void execute(PrintWriter out) {
         if (!(params.containsKey("id") && params.containsKey("instrument") && params.containsKey("price") && params.containsKey("quantity") && params.containsKey("type"))) {
             //pass 404 error...
-            out.println("404 Error!");
+            out.println("Mismatched parameters");
         } else {
+            if(params.get("id") == null || params.get("instrument") == null || params.get("price") == null ||params.get("quantity") == null){
+                out.println("Mismatched parameters");
+                return;
+            }
             switch (params.get("type")) {
                 case "GTC":
                     out.println(db.sell_gtc(Integer.parseInt(params.get("id")),params.get("instrument"),Integer.parseInt(params.get("price")),Integer.parseInt(params.get("quantity"))));
@@ -37,8 +63,12 @@ class Buy extends OrderHandler {
     public void execute(PrintWriter out) {
         if (!(params.containsKey("id") && params.containsKey("instrument") && params.containsKey("price") && params.containsKey("quantity") && params.containsKey("type"))) {
             //pass 404 error...
-            out.println("404 Error!");
+            out.println("Mismatched parameters");
         } else {
+            if(params.get("id") == null || params.get("instrument") == null || params.get("price") == null ||params.get("quantity") == null){
+                out.println("Mismatched parameters");
+                return;
+            }
             switch (params.get("type")) {
                 case "GTC":
                     out.println(db.buy_gtc(Integer.parseInt(params.get("id")),params.get("instrument"),Integer.parseInt(params.get("price")),Integer.parseInt(params.get("quantity"))));
@@ -63,9 +93,13 @@ class Add extends CustomerHandler {
 	public void execute(PrintWriter out) {
         if ( !(params.containsKey("id") && params.containsKey("name") && params.containsKey("family"))) {
             //pass 404 error...
-            out.println("404 Error!");
+            out.println("Mismatched parameters");
         }
         else {
+            if(params.get("id") == null || params.get("name") == null || params.get("family") == null){
+                    out.println("Mismatched parameters");
+                    return;
+                }
             if (db.add_customer(Integer.parseInt(params.get("id")),params.get("name"),params.get("family"))) {
                 out.println("New user is added");
             }
@@ -85,9 +119,13 @@ class Deposit extends CustomerHandler {
 	public void execute(PrintWriter out) {
         if (!(params.containsKey("id") && params.containsKey("amount"))) {
             //pass 404 error...
-            out.println("404 Error!");
+            out.println("Mismatched parameters");
         }
         else {
+            if(params.get("id") == null || params.get("name") == null || params.get("family") == null){
+                out.println("Mismatched parameters");
+                return;
+            }
             if (db.deposit_customer(Integer.parseInt(params.get("id")),Integer.parseInt(params.get("amount")))) {
                 out.println("Successful");
             }
@@ -105,9 +143,13 @@ class Withdraw extends CustomerHandler {
 	public void execute(PrintWriter out) {
         if (!(params.containsKey("id") && params.containsKey("amount"))) {
             //pass 404 error...
-            out.println("404 Error!");
+            out.println("Mismatched parameters");
         }
         else {
+            if(params.get("id") == null || params.get("name") == null || params.get("family") == null){
+                out.println("Mismatched parameters");
+                return;
+            }
             switch (db.withdraw_customer(Integer.parseInt(params.get("id")),Integer.parseInt(params.get("amount")))) {
                 case 0:
                     out.println("Successful");
@@ -135,6 +177,7 @@ public class CA2 {
 			server.createContext("/customer/add", new Add(db));
 			server.createContext("/customer/deposit", new Deposit(db));
 			server.createContext("/customer/withdraw", new Withdraw(db));
+            server.createContext("/",new ErrorHandler());
 			server.start();
 
 
--- src/CustomerHandler.java
+++ src/CustomerHandler.java
@@ -15,8 +15,28 @@ public abstract class CustomerHandler extends ServiceHandler {
 
         public abstract void execute(PrintWriter out) throws IOException;
 
-       /* public void handle(HttpExchange t) throws IOException {
+        public void sendResponse(HttpExchange t,byte[] result,int status) throws IOException{
+                t.sendResponseHeaders(status, result.length);
+                Headers headers = t.getResponseHeaders();
+                headers.add("Date", Calendar.getInstance().getTime().toString());
+                headers.add("Content-Type", "text/html");
+                OutputStream os = t.getResponseBody();
+                os.write(result);
+                os.close();
+        }
 
-        }*/
+        public void handle(HttpExchange t) throws IOException {
+                extractParams(t.getRequestURI().getQuery());
+                StringWriter sw = new StringWriter();
+                execute(new PrintWriter(sw, true));
+                String message = sw.toString();
+                byte[] result = message.getBytes();
+                if(message.equals("Mismatched parameters"))
+                        sendResponse(t,result,404);
+
+                else
+                        sendResponse(t,result,200);
+
+        }
 
 }
