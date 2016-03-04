f5dc306ea4fd2631df4f1e66075168b2e0be6a63
mohammad <mohi_6038550@yahoo.com>
2016-03-04 13:28:32 +0330

    ettepaghe mobarak

--- src/PageBuilder.java
+++ src/PageBuilder.java
@@ -5,8 +5,8 @@ import java.util.Map;
 
 public class PageBuilder {
 
-    public static void createPage(PrintWriter out,String adr,String title,String msg){
-        new Page("./index.html")
+    public static void createPage(PrintWriter out,String page,String adr,String title,String msg){
+        new Page("./" + page)
                 .subst("title",title)
                 .subst("adr", "\""  + adr + "\"" )
                 .subst("msg", msg )
